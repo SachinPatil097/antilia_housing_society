39985974be8e5a9577b9e552df7bc0c54164aea5
sachinp.tagline@gmail.com <sachinp.tagline@gmail.com>
2024-08-27 05:52:20 +0000

    Created main.Form2 and main; Edited user.LoginForm, settings and user.BaseForm; Deleted Form2 and user.RegistrationForm

--- client_code/user/BaseForm/__init__.py
+++ client_code/user/BaseForm/__init__.py
@@ -6,6 +6,5 @@ class BaseForm(BaseFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.label_welcome.text = "Welcome, User!"
 
     # Any code you write here will run before the form opens.
--- client_code/user/LoginForm/__init__.py
+++ client_code/user/LoginForm/__init__.py
@@ -8,3 +8,14 @@ class LoginForm(LoginFormTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def submit_registration_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    email = self.email_input.text
+    password = self.password_input.text
+    result = anvil.server.call('register_user', email, password)
+    if result['success']:
+        alert(result['message'])
+        self.open_form('LoginForm')
+    else:
+        alert(result['message'])
